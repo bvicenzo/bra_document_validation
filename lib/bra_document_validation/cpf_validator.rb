4d7d812a7a25d96aa4e93e02ab0d480b45fe4a78
Bruno Vicenzo <bruno@alumni.usp.br>
2022-03-31 16:44:17 -0300

    Refactory - CPF Validator using new bra_documents methods

--- lib/bra_document_validation/cpf_validator.rb
+++ lib/bra_document_validation/cpf_validator.rb
@@ -2,42 +2,24 @@
 
 module BraDocumentValidation
   class CPFValidator < ActiveModel::EachValidator
-    FORMATTED_CPF_PATTERN = /^(\d{3}\.\d{3}\.\d{3})-(\d{2})$/
-    RAW_CPF_PATTERN = /\A\d{11}\z/
-    NOT_NUMBER_PATTERN = /\D/
-
     def validate_each(record, attribute, value)
-      return record.errors.add(attribute, error_message(:invalid_format)) unless document_format.match?(value.to_s)
+      return record.errors.add(attribute, error_message(:invalid_format)) unless valid_format?(value)
 
-      full_number = only_numbers_for(value.to_s)
-
-      return unless black_listed?(full_number) || !digit_verified?(full_number)
-
-      record.errors.add(attribute, error_message(:invalid_verification_digit))
+      record.errors.add(attribute, error_message(:invalid_verification_digit)) unless valid_verification_digit?(value)
     end
 
     private
 
-    def digit_verified?(number)
-      person_number = number_without_verifying_digits_for(number)
-
-      number == BraDocuments::CPFGenerator.generate(person_number: person_number)
+    def valid_format?(document)
+      BraDocuments::Matcher.match?(document.to_s, kind: :cpf, mode: document_format)
     end
 
-    def number_without_verifying_digits_for(number)
-      number.chars.shift(BraDocuments::CPFGenerator::PERSON_NUMBER_SIZE).join
-    end
-
-    def only_numbers_for(value)
-      value.gsub(NOT_NUMBER_PATTERN, '')
-    end
-
-    def black_listed?(number)
-      number.chars.uniq.size == 1
+    def valid_verification_digit?(document)
+      BraDocuments::CPFGenerator.valid_verification_digit?(document: document.to_s)
     end
 
     def document_format
-      options[:formatted] ? FORMATTED_CPF_PATTERN : RAW_CPF_PATTERN
+      options[:formatted] ? :formatted : :raw
     end
 
     def error_message(default_message)
