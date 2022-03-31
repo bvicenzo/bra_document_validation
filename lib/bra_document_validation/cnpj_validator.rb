e4c24d8fac0553363bc6cd7951c79920126fd32d
Bruno Vicenzo <bruno@alumni.usp.br>
2022-03-31 16:47:56 -0300

    Refactory - CNPJ Validator using new bra_documents methods

--- lib/bra_document_validation/cnpj_validator.rb
+++ lib/bra_document_validation/cnpj_validator.rb
@@ -2,50 +2,24 @@
 
 module BraDocumentValidation
   class CNPJValidator < ActiveModel::EachValidator
-    FORMATTED_CNPJ_PATTERN = %r{\A(\d{2}\.\d{3}\.\d{3}/\d{4})-(\d{2})\Z}
-    RAW_CNPJ_PATTERN = /\A\d{14}\Z/
-    NOT_NUMBER_PATTERN = /\D/
-
     def validate_each(record, attribute, value)
-      return record.errors.add(attribute, error_message(:invalid_format)) unless document_format.match?(value.to_s)
+      return record.errors.add(attribute, error_message(:invalid_format)) unless valid_format?(value)
 
-      full_number = only_numbers_for(value.to_s)
-
-      return unless black_listed?(full_number) || !digit_verified?(full_number)
-
-      record.errors.add(attribute, error_message(:invalid_verification_digit))
+      record.errors.add(attribute, error_message(:invalid_verification_digit)) unless valid_verification_digit?(value)
     end
 
     private
 
-    def digit_verified?(full_number)
-      company_number, matrix_subsidiary_number = numbers_for(full_number)
-
-      full_number == BraDocuments::CNPJGenerator.generate(
-        company_number: company_number,
-        matrix_subsidiary_number: matrix_subsidiary_number
-      )
+    def valid_format?(document)
+      BraDocuments::Matcher.match?(document.to_s, kind: :cnpj, mode: document_format)
     end
 
-    def numbers_for(value)
-      number = value.gsub(NOT_NUMBER_PATTERN, '').chars
-
-      [
-        number.shift(BraDocuments::CNPJGenerator::COMPANY_NUMBER_SIZE).join,
-        number.shift(BraDocuments::CNPJGenerator::MATRIX_SUBSIDIARY_SIZE).join
-      ]
-    end
-
-    def only_numbers_for(value)
-      value.gsub(NOT_NUMBER_PATTERN, '')
-    end
-
-    def black_listed?(number)
-      number.chars.uniq.size == 1
+    def valid_verification_digit?(document)
+      BraDocuments::CNPJGenerator.valid_verification_digit?(document: document.to_s)
     end
 
     def document_format
-      options[:formatted] ? FORMATTED_CNPJ_PATTERN : RAW_CNPJ_PATTERN
+      options[:formatted] ? :formatted : :raw
     end
 
     def error_message(default_message)
